5ab910e7d931a51510741258cd6badbf64f60b79
Niks150 <sandeepswami344@gmail.com>
2026-07-13 11:20:23 +0530

    Truthy and falsy

--- L03-conditional/script.js
+++ L03-conditional/script.js
@@ -19,3 +19,61 @@
 
 //console.log(true === true); //true
 
+// != looze not equality operator
+
+// console.log(2 != "2"); // false
+
+//console.log(true != 0); // true
+
+// strict not equality operator !== checks both value and data type
+
+// console.log(2 !== "2"); // true
+
+// les than operation
+
+// console.log(2<3); // true
+// console.log(2<2); // false
+
+// console.log("2"<2); // false
+// console.log(5<true); // false
+// console.log(2<"45"); // true
+
+//greater than operation
+
+// console.log(2>3); // false
+// console.log(2>2); // false
+
+// console.log("2">2); // false
+// console.log(5>true); // true
+// console.log(2>"45"); // false
+
+// less than or equal to operation
+
+// console.log(2<=3); // true
+// console.log(2<=2); // true   
+
+// console.log("2"<=2); // true
+
+//greater than or equal to operation
+
+// console.log(2>=3); // false
+// console.log(2>=2); // true
+// console.log("2">=2); // true
+
+// logical operators    
+// logical AND operator &&
+
+// In js ebery value has a truthy or falsy value.
+// falsy values are false, 0, "", null, undefined, NaN
+// truthy values are all other values except falsy values
+
+//console.log(Boolean({})); // true
+// console.log(Boolean("")); // false
+// console.log(Boolean(null)); // false
+// console.log(Boolean(undefined)); // false
+// console.log(Boolean(NaN)); // false
+
+
+
+
+
