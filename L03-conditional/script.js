20d5e1a193578408f7fce2298be98d3cb58cddda
Niks150 <sandeepswami344@gmail.com>
2026-07-13 10:36:56 +0530

    Conditional

---
+++ L03-conditional/script.js
@@ -0,0 +1,21 @@
+// Conditional Statements in JavaScript
+
+// == checks only the value and not the data type its is called loose equality operator
+// console.log(2=="2");
+
+// line a1
+
+//console.log(1234 == "1234"); // true
+
+//console.log(2 == ""); // false
+
+// when we compare boolean with number it will convert the boolean to number and then compare
+// console.log(true == 1); // true
+
+//console.log(1==1.0); // true
+
+// strict equality operator === checks both value and data type
+// console.log(2==="2"); // false
+
+//console.log(true === true); //true
+
---
+++ forme.js
@@ -0,0 +1,32 @@
+/* const name = "sandeep"
+console.log(`Hello my name is ${name}`); // we use backtags for the string interpolation
+
+// Fahrenheit = (Celsius × 9/5) + 32
+ const celsius = 30;
+ const fahrenheit = (celsius * 9/5) + 32;
+ console.log(fahrenheit);
+
+ ------------Number------------
+ 
+ const hundered = 10000000;
+ console.log(hundered.toLocaleString()); // 10,000,000
+
+ console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
+ console.log(Number.MIN_VALUE); // 5e-324
+
+ console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
+
+ console.log(isNaN("hello")); // true
+ console.log(Number.isNaN("sandeep ")); // false
+
+ */
+
+ //----------------Math-----------------
+
+// console.log(Math.ceil(a)); // 26
+// console.log(Math.floor(4.4)); //4
+
+
+let a;
+a=a+5;
+console.log(a);
