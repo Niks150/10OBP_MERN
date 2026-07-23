d6153d6ada7f419287d9ccdfe57bff2d11a94203
Niks150 <sandeepswami344@gmail.com>
2026-07-23 23:44:00 +0530

    New

--- L06-Array/array.js
+++ L06-Array/array.js
@@ -1,14 +1,22 @@
 // let Arr = [1,2,3,4];
 // console.log(Arr[2]);
 
-let student = [1,2,3,4];
-let sum = 0;
-for(let i = 0;i<student.length;i++){
+// let student = [1,2,3,4];
+// let sum = 0;
+// for(let i = 0;i<student.length;i++){
     
-     sum += student[i];
+//      sum += student[i];
     
-}
-console.log(sum);
+// }
+// console.log(sum);
 
 // Hw:- print the array into reverse
-// usu
+
+// const arr = [1,2,3,4]
+
+// arr.reverse();
+
+// console.log(arr);
+
+
+
---
+++ Practice/Function.js
@@ -0,0 +1,32 @@
+// In this will learn how to solve all the program using the function 
+
+// function sum(a,b){         //sum of two variable using the function 
+//     console.log(a+b);
+// }
+// sum(2,3)
+// sum(100,200)
+
+// function isEven(n){
+//     if(n%2==0){
+//         return 1;        // isEven or odd using the functions
+//     }
+//     else{
+//         return 0;
+//     }
+// }
+
+// console.log(isEven(1));
+
+// function sum(){
+//     let a = 20,b=30;
+//     if(a==20){
+//         console.log(`a is ${a}`);
+//     }
+//     return a,b;
+
+    
+// }
+// sum()
+
+//How can we take the user input in java script using the readline
+
