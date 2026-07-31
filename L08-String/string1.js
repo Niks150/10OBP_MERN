cafb97462bb30f07a535aa61a0699f0a93624d94
Niks150 <sandeepswami344@gmail.com>
2026-07-31 12:11:34 +0530

    New Folder add

---
+++ L08-String/string1.js
@@ -0,0 +1,70 @@
+/*===================STRING================*/
+
+// let str1 ="Hello"
+// console.log(str1)
+
+// let str2 = "There we are starting"
+// console.log(str2)
+
+// let str3 = ""
+// console.log(`string${str3}`)
+
+// how to access the string 
+
+//we have two approach to access the string 
+
+//1. charAt it is a function with which we can access the string
+
+//let str = "Sandeep" 
+//console.log(str.charAt(2)); // n
+
+//2. with the use of Index value 
+
+// let str = "Niku"
+// console.log(str[2]) //K
+
+//How to find length of the following String 
+
+// let str = "Niku"
+// console.log(str.length) //4
+
+
+
+//String InterPolation  //template literals
+// let str = "Niku"
+// console.log(`Hello there my nick name is ${str}`) //Hello there my nick name is Niku
+
+//Question 1 :- 
+
+// let marks = 90;
+// let str = `My marks are ${marks}`
+// console.log(str)
+
+//Question 2 :-
+
+//Immutable :- String in javaScript is Immutable : we cannot change the value on the run time 
+
+// let str = "Hello"
+// //Now i have to replace the value e to T
+// str[1] = "t"
+// console.log(str) //Hello so the we can say that e is not replace to t 
+
+//String concatenation  (+)
+
+// let a = "12"
+// let b ="12"
+// console.log(a*b)
+
+
+
+// let str = "sandeep"
+
+// function revesreString(str){
+//     let rev=''
+//     for(let i = str.length-1 ; i>=0; i--){
+//         rev += str[i]
+//     }    
+//     return rev
+// }
+// console.log(revesreString(str))
+
